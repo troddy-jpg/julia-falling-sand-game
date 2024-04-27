2313969e3fb06a73f998737d8cdfa09e14daaee9
ashkaty <ashkaty@gmail.com>
2024-04-27 16:57:21 -0500

    Created Julia project and created basic grid

---
+++ SandGame/src/SandGame.jl
@@ -0,0 +1,9 @@
+module SandGame
+using Revise
+using GameZero
+
+#in order to run the game, you need to either click the play button on the top right in VSCode or run the following command in the terminal
+
+rungame("C:\\Users\\ashka\\Documents\\Coding\\julia-falling-sand-game\\SandGame\\src\\grid.jl")
+
+end # module SandGame
